fbc5937616cb252168d907b1ac0171ccee4eb3e7
shra14600 <104187381+shra14600@users.noreply.github.com>
2022-07-17 16:55:08 +0530

    to do list with JS

---
+++ javascript.js
@@ -0,0 +1,27 @@
+const input = document.querySelector("#input");
+const addBtn = document.querySelector("#add-btn");
+const ul = document.querySelector("ul");
+const delBtn = document.querySelector(".fa-trash");
+
+function deleteTask(){
+  this.style.display = 'none';
+}
+
+const addTask = () => {
+  let inputVal = input.value;
+  if (inputVal !== "") {
+    var li = document.createElement("li");
+    li.innerHTML = `${inputVal}<i class="fas fa-check fa-trash">`
+    li.addEventListener("click", deleteTask);
+    ul.appendChild(li);
+    input.value = "";
+  }
+};
+
+input.addEventListener("change", () => {
+  addTask();
+});
+
+addBtn.addEventListener("click", () => {
+  addTask();
+});
